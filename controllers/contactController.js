74d2e7c88e8735c5378abbeb240e5b59825189ae
Irteza Asad <irtezaasad.sheba@gmail.com>
2020-02-02 19:38:52 +0600

    phonebook restAPI [INIT]

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -15,24 +15,19 @@ exports.index = function (req, res) {
         });
     });
 };
-
 exports.new = function (req, res) {
     var contact = new Contact();
     contact.name = req.body.name ? req.body.name : contact.name;
     contact.gender = req.body.gender;
     contact.email = req.body.email;
     contact.phone = req.body.phone;
-// save the contact and check for errors
     contact.save(function (err) {
-        // if (err)
-        //     res.json(err);
         res.json({
             message: 'New contact created!',
             data: contact
         });
     });
 };
-
 exports.view = function (req, res) {
     Contact.findById(req.params.contact_id, function (err, contact) {
         if (err)
@@ -43,7 +38,6 @@ exports.view = function (req, res) {
         });
     });
 };
-
 exports.update = function (req, res) {
     Contact.findById(req.params.contact_id, function (err, contact) {
         if (err)
@@ -52,7 +46,6 @@ exports.update = function (req, res) {
         contact.gender = req.body.gender;
         contact.email = req.body.email;
         contact.phone = req.body.phone;
-// save the contact and check for errors
         contact.save(function (err) {
             if (err)
                 res.json(err);
@@ -63,7 +56,6 @@ exports.update = function (req, res) {
         });
     });
 };
-
 exports.delete = function (req, res) {
     Contact.remove({
         _id: req.params.contact_id
